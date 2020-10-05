47a9e26ab758b8a19e4cb4a6f16775532ad87659
WillDevAC <wydemairomsilva@gmail.com>
2020-10-05 14:33:11 -0300

    Fixed Emaill

--- ajax/API.js
+++ ajax/API.js
@@ -3,16 +3,5 @@ function ola(){
 	var email = $('#email').val();
 	var nome = $('#mensagem').val();
 
-	$.ajax({
-			url: 'https://roxytech.com.br/ajax/email.php',
-			type: 'post',
-			data: {nome: nome, email: email, mensagem: mensagem},
-			dataType: 'json',
-			success: function (data) {
-				$('#nome').val("");
-				$('#email').val("");
-				$('#mensagem').val("");
-				$('#sucesso').val("EMAIL ENVIADO COM SUCESSO!");
-			}
-		});
+	alert(nome);
 }
