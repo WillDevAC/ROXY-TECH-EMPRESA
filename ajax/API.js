cd7ea6bf50fddccffab1c084f20ee8fb795b1572
WillDevAC <wydemairomsilva@gmail.com>
2020-10-06 02:09:50 -0300

    email-fixeed

--- ajax/API.js
+++
@@ -1,7 +0,0 @@
-function ola(){
-	var nome = $('#nome').val();
-	var email = $('#email').val();
-	var nome = $('#mensagem').val();
-
-	alert(nome);
-}
---
+++ email.js
@@ -0,0 +1,36 @@
+//Inserindo requisição no banco
+$('#form1').submit(function(e){
+	e.preventDefault();
+
+	var nome = $('#nome').val();
+	var email = $('#email').val();
+	var mensagem = $('#mensagem').val();
+
+	$.ajax({
+		url: 'http://radiocampusapi.com.br/roxytech/email/send',
+		type: 'GET',
+		dataType: 'json',
+		data: {mensagem: 'CLIENTE: '+ nome + ' || EMAIL:  '+ email + ' || PROJETO DETALHADO: ' + mensagem + ''},
+		  beforeSend: function() {
+		  	$('#carregando').text("AGUARDE! ENVIANDO SOLICITAÇÃO...");
+		},
+	})
+	.done(function() {
+		$('#nome').val("");
+		$('#email').val("");
+		$('#mensagem').val("");
+		$('#carregando').text("");
+		$('#sucesso').text("SUCESSO! EM BREVE ENTRAREMOS EM CONTATO.");
+	})
+	.fail(function() {
+		$('#nome').val("");
+		$('#email').val("");
+		$('#mensagem').val("");
+		$('#carregando').text("");
+		$('#falha').text("FALHA AO ENVIAR EMAIL!");
+	})
+	
+});
+
+
+
